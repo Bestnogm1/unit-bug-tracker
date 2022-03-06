a37fb67ba9b1f2656ddb52af002018bd3c284fe6
Bestnogm1 <koffiarielhessou@gmail.com>
2022-03-06 10:18:31 -0500

    add foreach to the ticket/ index

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,26 +1,21 @@
 import { Ticket } from '../models/tickets.js'
 
 
-// function index(req, res) {
-//   Ticket.find({}) 
-//   .then(ticket =>{
-//     res.render('tickets/index', {
-//       ticket,
-//       error,
-//       title:"helolo"
-//     })
-//   })
-  
-// }
 function index(req, res) {
-  Ticket.find({},function(error, tickets){
-    res.render('tickets/index',{
-      error,
-      title:"wewe",
-      tickets
+  Ticket.find({}) 
+  .then(tickets =>{
+    res.render('tickets/index', {
+      tickets,
+      title:"helolo"
     })
   })
+  .catch(err =>{
+    console.log(err);
+    re.redirect('/tickets')
+  })
+  
 }
+
 export{
   index
 }
