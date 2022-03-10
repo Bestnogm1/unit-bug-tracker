c09ad78543961ffce8fb5a53078e4b288ac03929
Bestnogm1 <koffiarielhessou@gmail.com>
2022-03-10 13:54:39 -0500

    adding profile

---
+++ routes/profiles.js
@@ -0,0 +1,4 @@
+import { Router } from 'express'
+import * as profilesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from '../middleware/middleware.js'
+const router = Router()
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ import('./config/passport.js')
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as ticketRouter } from './routes/tickets.js'
+import { router as profilesRouter } from './routes/profiles.js'
 import { router as commentRouter } from './routes/comments.js'
 // create the express app
 const app = express()
@@ -65,6 +66,7 @@ app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use('/tickets', ticketRouter)
 app.use('/comments',commentRouter)
+app.use('/profiles', profilesRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
